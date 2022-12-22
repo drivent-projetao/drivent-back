9365fd568dd9c5828caa2bb82cde8d67ca1428b2
LucasPCotrim <lucaspcotrim@gmail.com>
2022-12-21 21:58:02 -0300

    fix: Corrected route GET /activities/dates

--- src/utils/formatDate.ts
+++ src/utils/formatDate.ts
@@ -4,7 +4,8 @@ function formatDateWithWeekday(date: Date) {
   const dayIndex = Number(date.getDay());
   const month = String(date.getUTCMonth() + 1).padStart(2, "0");
   const day = String(date.getUTCDate()).padStart(2, "0");
-  const dateString = `${WEEKDAYS[dayIndex]}, ${day}/${month}`;
+  const year = String(date.getUTCFullYear());
+  const dateString = `${WEEKDAYS[dayIndex]}, ${year}/${month}/${day}`;
   return dateString;
 }
 
